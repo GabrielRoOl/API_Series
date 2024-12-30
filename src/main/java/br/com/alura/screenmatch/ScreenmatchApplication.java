9bbd1ea36c29c6c6ba72e2225d7c08ca8acfe43f
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-12-30 15:28:32 -0300

    Update Principal

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,14 +1,10 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import br.com.alura.screenmatch.model.DadosEpisodios;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,37 +19,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		var consumoApi = new ConsumoApi();
+		Principal principal = new Principal();
 
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=2def0c3f");
+		principal.exibeMenuI();
 
-		/*
-		 * System.out.println("\n" + json + "\n");
-		 * json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		 */
 
-		// System.out.println("\n" + json + "\n");
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-
-		System.out.println("\n" + dados + "\n");
-
-		json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=2def0c3f");
-
-		ConverteDados converterSerie = new ConverteDados();
-		DadosEpisodios dadosEpisodios = converterSerie.obterDados(json, DadosEpisodios.class);
-
-		System.out.println("\n" + dadosEpisodios + "\n");
-
-        List<DadosTemporada> temporada = new ArrayList<>();
-
-        for (int i = 1; i <= dados.totalTemporadas(); i++){
-            json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=2def0c3f");
-            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-            temporada.add(dadosTemporada);
-        }
-        temporada.forEach(System.out::println);
 	}
+
 
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,13 +1,19 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner sc = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados converte = new ConverteDados();
 
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=2def0c3f";
@@ -15,13 +21,25 @@ public class Principal {
 
     public void exibeMenuI(){
 
-        System.out.println("Digite o nome da série para busca.");
+        System.out.print("Digite o nome da série para busca: ");
 
         var nomeSerie = sc.nextLine();
 
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
 
+        DadosSerie dados = converte.obterDados(json, DadosSerie.class);
+        System.out.println(dados + "\n");
+
+        List<DadosTemporada> temporada = new ArrayList<>();
+
+        for (int i = 1; i <= dados.totalTemporadas(); i++){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = converte.obterDados(json, DadosTemporada.class);
+            temporada.add(dadosTemporada);
+        }
+        temporada.forEach(System.out::println);
 
 
     }
+
 }
