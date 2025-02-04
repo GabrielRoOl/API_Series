449a255cc45ad27854da43128cc5b35596931618
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-04 15:32:04 -0300

    feat(serie): adiciona service e controller

--- src/main/java/br/com/alura/screenmatch/controllers/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controllers/SerieController.java
@@ -1,28 +1,24 @@
 package br.com.alura.screenmatch.controllers;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.Serie;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/series")
 public class SerieController {
 
     @Autowired
-    private SerieRepository serieRepository;
+    private SerieService serieService;
 
     @GetMapping
     public List<SerieDTO> findAll(){
-        List<Serie> result = serieRepository.findAll();
-        return result.stream().map(SerieDTO::new).collect(Collectors.toList());
+        return serieService.findAll();
     }
 
 }
---
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -0,0 +1,23 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    SerieRepository serieRepository;
+
+    public List<SerieDTO> findAll(){
+        List<Serie> result = serieRepository.findAll();
+        return result.stream().map(SerieDTO::new).collect(Collectors.toList());
+    }
+
+}
